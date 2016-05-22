2740d7d23333eb788a60dfffb488a9046e4596cc
Dan <dan@dan.com>
2016-05-22 12:11:32 -0500

    updated

---
+++ create_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import sys,os
+
+if len(sys.argv) != 2:
+    print("Only one argument should be given, the number of commits to make.")
+    sys.exit(1)
+
+num_commits = int(sys.argv[1])
+print("Creating %d commits" % int(sys.argv[1]))
+
+for i in range(0,num_commits):
+    os.system('echo blah >> a.txt')
+
+
